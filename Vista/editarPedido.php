ce2c2cd8a8cba4a8c8621420f0787cdc7054747f
farly londono <farlylondono@gmail.com>
2020-12-05 15:00:19 -0500

    icono editarpedido

--- Vista/editarPedido.php
+++ Vista/editarPedido.php
@@ -28,6 +28,7 @@ $buscarPedido= $ControladorPedido->buscarPedido($_GET["idPedido"]);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EditarPedido</title>
+    <link rel="icon" type="image/png" href="../Img/hamburguer.png" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
         integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 </head>
