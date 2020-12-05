2ac268ed69ce6a5b509898f3fbd3ccf9d31fb03b
farly londono <farlylondono@gmail.com>
2020-12-05 14:56:41 -0500

    icono registrarpedido

--- Vista/RegistrarPedido.php
+++ Vista/RegistrarPedido.php
@@ -22,6 +22,7 @@ $listarProductos = $ControladorPedido->listarProductos();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registrar Pedido</title>
+    <link rel="icon" type="image/png" href="../Img/hamburguer.png" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
         integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
