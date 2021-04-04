017f2157482c1d65fb4f587981fd66c600b2b7de
farly londono <farlylondono@gmail.com>
2021-04-04 11:48:28 -0500

    ajustes

--- Vista/listarProductosimagen.php
+++ Vista/listarProductosimagen.php
@@ -26,11 +26,8 @@ function desplegarVista2($ruta){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Listado de Productos</title>
     <link rel="icon" type="image/png" href="../Img/hamburguer.png" />
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
+    
     
 </head>
 <body background="../Img/rsz_jaco-pretorius-agzehyx-jfo-unsplash_1.jpg">
@@ -38,9 +35,7 @@ function desplegarVista2($ruta){
         <div class="card text-white bg-secondary mb-3">
         <p class="h1" align="center">Lista Productos</p>  
     </div>
-    <a href="menu.php" class="btn btn-success" >REGRESAR</a>
-    <a  href="Vista/ListaCarritoCompras.php"  ><img src="Img/outline_shopping_cart_white_36dp.png" class=" my-3 my-sm-0 mr-sm-3" width="50" height="50" style="color:red;"></a> 
-    <br>
+    
     <br>
     <?php if($mensaje != "") { ?>
         <div class="alert alert-success">
