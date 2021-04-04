68180b3399f2fa7b5c864a10adbde258fe8a8bac
farly londono <farlylondono@gmail.com>
2021-04-04 11:44:25 -0500

    ajustes

--- Vista/listarProductosimagen.php
+++ Vista/listarProductosimagen.php
@@ -26,10 +26,10 @@ function desplegarVista2($ruta){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Listado de Productos</title>
     <link rel="icon" type="image/png" href="../Img/hamburguer.png" />
-    <link rel="icon" type="image/png" href="../Img/hamburguer.png" />
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
     
 </head>
@@ -52,8 +52,8 @@ function desplegarVista2($ruta){
     <?php  } ?>
     <div class="row"> 
         <?php foreach($listarProductos as $key=>$C){  ?>
-            <div class="col-3">
-                    <div class="card">
+            <div class="col-3"  >
+                    <div class="card" >
                         <img
                         title="<?php echo $C->getNombreProducto(); ?>" 
                         alt="<?php  echo $C->getNombreProducto(); ?>"
@@ -62,11 +62,12 @@ function desplegarVista2($ruta){
                         data-trigger="hover"
                         data-content="<?php  echo $C->getDescripcionProducto(); ?>"
                         height="250px"
-                        
                         >
                         <div class="card-body">
                             <span><strong><?php echo $C->getNombreProducto(); ?></strong></span>
+                            <p class="card-text"><?php  echo $C->getDescripcionProducto(); ?></p>
                             <h5 class="card-title"><strong style="color:green;"><?php  echo $C->getPrecioProducto(); ?></strong></h5>
+                            
                             
                            
 
