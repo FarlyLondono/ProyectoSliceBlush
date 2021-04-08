040b1487da4a4daeca15aec0527282c070ea2446
Julian h <julianhs1994@gmail.com>
2021-04-07 19:11:53 -0500

    Actualizacion Recuperar Contraseña

---
+++ Controlador/Correo.php
@@ -0,0 +1,50 @@
+<?php
+require_once("../Controlador/controlador.php");
+$crudcliente = new CRUDcliente();
+
+if(isset($_POST['recuperarContra'])){
+    if(!empty($_POST['Correo'])){
+    $Correo = $_POST['Correo'];
+    $NombreCorreo = $crudcliente->buscarCorreoCliente($Correo);
+        if ($NombreCorreo == 1){
+            $Contrasena = $crudcliente->buscarContrasena($Correo);
+            if($Contrasena <> ""){
+                $para = 'julianhs1994@gmail.com';
+                $titulo = 'recuperacion contra';
+                $mensaje =  'hola';
+                $cabeceras = 'From: webmaster@example.com' . "\r\n" .
+                         'Reply-To: webmaster@example.com' . "\r\n" .
+                         'X-Mailer: PHP/' . phpversion();
+                         
+                ini_set("SMTP","smtp.gmail.com");         
+                ini_set('sendmail_from','SliceBlushTest@gmail.com');
+                mail($para,$titulo,$mensaje,$cabeceras);
+                echo "Realizado";
+            }else{
+                echo "Ha ocurrido un error!"; 
+                "<script src='https://code.jquery.com/jquery-3.5.1.js'></script>";
+    "<script src='https://cdn.jsdelivr.net/npm/sweetalert2@9'></script>";
+    echo "<script>
+    Swal.fire({
+        position: 'top-end',
+        icon: 'error',
+        title: 'Problemas...'
+    });
+    </script>";
+            }
+        }else{
+            echo "<h2>Este correo NO se ha registrado!</h2>";
+            "<script src='https://code.jquery.com/jquery-3.5.1.js'></script>";
+    "<script src='https://cdn.jsdelivr.net/npm/sweetalert2@9'></script>";
+    echo "<script>
+    Swal.fire({
+        position: 'top-end',
+        icon: 'error',
+        title: 'Problemas...'
+    });
+    </script>";
+        }
+    }
+}
+
+?>
--- js/validarrecuperacion.js
+++ js/validarrecuperacion.js
@@ -2,23 +2,24 @@ function validanDatos() {
 
     var correo = document.getElementsByName('Correo')[0].value;
 
-    if ((correo == "")) { //COMPRUEBA CAMPOS VACIOS
-        swal({
+    if (correo == "") { //COMPRUEBA CAMPOS VACIOS
+        /*swal({
             icon: 'error',
             title: 'El campo correo es obligatorio'
-        });
+        });*/
+        alert("El campo correo es obligatorio")
         return true;
     }
     var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
     //var address = document.getElementById[email].value;
     if (reg.test(correo) == false) {
-        swal('Correo invalido');
+        //swal('Correo invalido');
         return (false);
     } else {
-        swal("Se envio un correo de recuperacion.", {
+       /* swal("Se envio un correo de recuperacion.", {
             button: "OK"
         }).then(function() {
             window.location.href = "../index.php"
-        })
+        })*/
     }
 }
